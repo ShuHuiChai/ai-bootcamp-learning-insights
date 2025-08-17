df34d87d6a1065b9a97906b397ddf6578f08f2e7
ShuHuiChai <125629700+ShuHuiChai@users.noreply.github.com>
2025-08-18 02:58:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
         llm = ChatOpenAI(
             model='gpt-4o-mini',
             temperature=0,
-            api_key=st.secrets('OPENAI'),
+            api_key=st.secrets['OPENAI'],
         )
 
     greeting = """Hello! I am your friendly Intelligent Insight Agent, here to help you analyse your students' performance on assignments in \
