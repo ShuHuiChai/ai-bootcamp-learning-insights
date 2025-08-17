c2b32e0ab991c32228e8f73dd1304080419f449e
ShuHuiChai <125629700+ShuHuiChai@users.noreply.github.com>
2025-08-18 02:42:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 def main():
     
     # region <--------- Streamlit Page Configuration --------->
